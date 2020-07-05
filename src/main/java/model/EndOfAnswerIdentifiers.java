7023f4b17221fa4ad4b7062104dce25a21cf9611
Artiom U <arti.urb@gmail.com>
2020-07-05 13:44:40 +0200

    Initial commit

---
+++ src/main/java/model/EndOfAnswerIdentifiers.java
@@ -0,0 +1,7 @@
+package model;
+
+public class EndOfAnswerIdentifiers extends RuntimeException {
+    public EndOfAnswerIdentifiers(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/model/LoaderException.java
@@ -0,0 +1,8 @@
+package model;
+
+public class LoaderException extends RuntimeException{
+    public LoaderException(String s) {
+        super (s);
+
+    }
+}
